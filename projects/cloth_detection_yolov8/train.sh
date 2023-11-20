b89500d693859c8b7571e96295902db475b78235
shuai.he <shuai.he@ur.com.cn>
2023-11-20 14:53:26 +0800

    inital commit

---
+++ projects/cloth_detection_yolov8/train.sh
@@ -0,0 +1,6 @@
+yolo detect train data=fashionpedia.yaml \
+  model=yolov8m.yaml \
+  pretrained=/home/justsomeone/code/AlgoQOL/algo_qol/algo/detection/ultralytics/runs/detect/train3/weights/best.pt \
+  epochs=100 \
+  imgsz=640 \
+  batch=48
